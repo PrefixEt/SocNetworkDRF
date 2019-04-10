a5bfce66826f5eda1af3fbae5c75c6a4d66458a0
kornev <kornev@specialist-it.com.ua>
2019-04-10 07:50:47 +0300

    user functional

--- user/views.py
+++ user/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.signals import user_logged_in
+
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework_jwt.utils import jwt_payload_handler, jwt
+from rest_framework.generics import RetrieveUpdateAPIView
+from api import settings
 from .serializers import UserSerializer
+from .models import User
+
+
 
 class CreateUsersAPIView(APIView):
     permission_classes = (AllowAny,)
@@ -15,6 +24,53 @@ class CreateUsersAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-
+@api_view(['POST'])
+@permission_classes([AllowAny, ])
+def autentification_user(request): 
+    try:
+        email = request.data['email']
+        password = request.data['password']
+        user = User.objects.get(email=email, password=password)    
+        if user:
+            try:
+                payload = jwt_payload_handler(user)
+                token = jwt.encode(payload, settings.SECRET_KEY)
+                user_details = {
+                    'name': '%s %s' % (user.first_name, user.last_name),
+                    'token': token
+                }
+                user_logged_in.send(sender=user.__class__, request=request, user=user)
+                return Response(user_details, status=status.HTTP_200_OK)
+            except Exception as e:
+                raise e
+        else:
+            res={'error':'can not authenticate'}
+            return Response(res, status=status.HTTP_403_FORBIDDEN)
+    except KeyError:
+        res ={'error':'please provide a email and a password'}
+        return Response(res)
         
 
+class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+ 
+    # Allow only authenticated users to access this url
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+ 
+    def get(self, request, *args, **kwargs):
+        # serializer to handle turning our `User` object into something that
+        # can be JSONified and sent to the client.
+        serializer = self.serializer_class(request.user)
+ 
+        return Response(serializer.data, status=status.HTTP_200_OK)
+ 
+    def put(self, request, *args, **kwargs):
+        serializer_data = request.data.get('user', {})
+ 
+        serializer = UserSerializer(
+            request.user, data=serializer_data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+ 
+        return Response(serializer.data, status=status.HTTP_200_OK)
